7d466c1962e59188804397bb7239f458a54f9a07
Andriy Makarevych <andriy@makarevych.com>
2023-02-17 03:04:08 +0100

    ...

--- src/main/java/giantvoid/dosbox/ezconfig/gui/GuiUtils.java
+++ src/main/java/giantvoid/dosbox/ezconfig/gui/GuiUtils.java
@@ -9,14 +9,26 @@ import java.awt.*;
 public class GuiUtils {
     private static final Font DEFAULT_FONT = new FontUIResource("Helvetica", Font.PLAIN, 12);
 
-    public static JPanel createSinglePropertyPanel(String imageIconName, JComponent propertyComponent) {
-        propertyComponent.setFont(DEFAULT_FONT);
-
+    public static JPanel createPropertyPanel(String imageIconName, JComboBox comboBox, JCheckBox checkBox) {
         JPanel groupPanel = new JPanel(new BorderLayout(10, 0));
         groupPanel.setBorder(BorderFactory.createEmptyBorder(10, 5, 0, 5));
 
         groupPanel.add(new JLabel(Props.getImageIcon(imageIconName)), BorderLayout.WEST);
-        groupPanel.add(propertyComponent, BorderLayout.CENTER);
+
+        comboBox.setFont(DEFAULT_FONT);
+
+        if (checkBox != null) {
+            checkBox.setFont(DEFAULT_FONT);
+            checkBox.setMargin(new Insets(0, 15, 0, 0));
+            JPanel selectionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+            selectionPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+            groupPanel.add(selectionPanel, BorderLayout.CENTER);
+
+            selectionPanel.add(comboBox);
+            selectionPanel.add(checkBox);
+        } else {
+            groupPanel.add(comboBox, BorderLayout.CENTER);
+        }
 
         return groupPanel;
     }
--- src/main/java/giantvoid/dosbox/ezconfig/gui/MachinePropertiesPanel.java
+++ src/main/java/giantvoid/dosbox/ezconfig/gui/MachinePropertiesPanel.java
@@ -1,7 +1,5 @@
 package giantvoid.dosbox.ezconfig.gui;
 
-import giantvoid.dosbox.ezconfig.base.Props;
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,8 +7,11 @@ public class MachinePropertiesPanel extends JPanel {
     private JComboBox<Item> comboBoxCpu;
     private JComboBox<Item> comboBoxMemory;
     private JComboBox<Item> comboBoxDisplay;
+    private JComboBox<Item> comboBoxKeyboardLayout;
+    private JComboBox<Item> comboBoxMouseSensitivity;
 
     private JCheckBox checkBoxFullscreen;
+    private JCheckBox checkBoxMouseAutoLock;
 
     public MachinePropertiesPanel() {
         super(new BorderLayout());
@@ -18,20 +19,26 @@ public class MachinePropertiesPanel extends JPanel {
     }
 
     private void initialize() {
-        JPanel contentPanel = new JPanel(new GridLayout(2, 2, 20, 0));
+        JPanel contentPanel = new JPanel(new GridLayout(3, 2, 20, 0));
         add(contentPanel, BorderLayout.NORTH);
 
         comboBoxCpu = new JComboBox<>(new Item[]{new Item("80386", "386")});
-        contentPanel.add(GuiUtils.createSinglePropertyPanel("cpu", comboBoxCpu));
+        contentPanel.add(GuiUtils.createPropertyPanel("cpu", comboBoxCpu, null));
 
-        comboBoxDisplay = new JComboBox<>(new Item[]{new Item("VGA", "vgaonly")});
-        contentPanel.add(GuiUtils.createSinglePropertyPanel("display", comboBoxDisplay));
+        comboBoxKeyboardLayout = new JComboBox<>(new Item[]{new Item("de - German", "de")});
+        contentPanel.add(GuiUtils.createPropertyPanel("keyboard", comboBoxKeyboardLayout, null));
 
         comboBoxMemory = new JComboBox<>(new Item[]{new Item("4MB", "4")});
-        contentPanel.add(GuiUtils.createSinglePropertyPanel("memory", comboBoxMemory));
+        contentPanel.add(GuiUtils.createPropertyPanel("memory", comboBoxMemory, null));
 
+        comboBoxMouseSensitivity = new JComboBox<>(new Item[]{new Item("normal", "500")});
+        checkBoxMouseAutoLock = new JCheckBox("Autolock");
+        checkBoxMouseAutoLock.setMargin(new Insets(0, 0, 0, 0));
+        contentPanel.add(GuiUtils.createPropertyPanel("mouse", comboBoxMouseSensitivity, checkBoxMouseAutoLock));
+
+        comboBoxDisplay = new JComboBox<>(new Item[]{new Item("VGA", "vgaonly")});
         checkBoxFullscreen = new JCheckBox("Fullscreen");
         checkBoxFullscreen.setMargin(new Insets(0, 0, 0, 0));
-        contentPanel.add(GuiUtils.createSinglePropertyPanel("empty", checkBoxFullscreen));
+        contentPanel.add(GuiUtils.createPropertyPanel("display", comboBoxDisplay, checkBoxFullscreen));
     }
 }
